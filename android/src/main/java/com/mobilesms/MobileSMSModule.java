8ce8bd3f15399add2712aa5d76e991e3f31437a0
bingisainath <bingisainath207@gmail.com>
2024-06-10 23:30:03 +0530

    returning the promise

--- android/src/main/java/com/mobilesms/MobileSMSModule.java
+++ android/src/main/java/com/mobilesms/MobileSMSModule.java
@@ -1,32 +1,49 @@
 package com.mobilesms;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.uimanager.IllegalViewOperationException;
 import android.telephony.SmsManager;
- 
+import android.util.Log;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 public class MobileSMSModule extends ReactContextBaseJavaModule {
- 
+
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+
     public MobileSMSModule(ReactApplicationContext reactContext) {
-        super(reactContext); //required by React Native
+        super(reactContext); // required by React Native
     }
- 
+
     @Override
-    //getName is required to define the name of the module represented in JavaScript
-    public String getName() { 
+    public String getName() {
         return "MobileSms";
     }
- 
+
     @ReactMethod
-    public void sendDirectSms(String phoneNumber, String msg) {
-        try {      
+    public void sendDirectSms(String phoneNumber, String msg, Promise promise) {
+        if (ContextCompat.checkSelfPermission(getCurrentActivity(), android.Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d("MobileSMSModule", "Permission not granted. Requesting permission.");
+            promise.reject("PERMISSION_NOT_GRANTED", "Permission not granted");
+            ActivityCompat.requestPermissions(getCurrentActivity(), new String[]{android.Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
+            return;
+        }
+
+        try {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNumber, null, msg, null, null);
-            System.out.println("message sent success.");
+            Log.d("MobileSMSModule", "Message sent successfully to " + phoneNumber);
+            promise.resolve("Message sent successfully");
+        } catch (IllegalArgumentException e) {
+            Log.e("MobileSMSModule", "Invalid phone number or message: " + e.getMessage());
+            promise.reject("INVALID_INPUT", "Invalid phone number or message: " + e.getMessage());
         } catch (Exception ex) {
-            System.out.println("couldn't send message.");
-        } 
+            Log.e("MobileSMSModule", "Couldn't send message: " + ex.getMessage());
+            promise.reject("SEND_ERROR", "Couldn't send message: " + ex.getMessage());
+        }
     }
 }
